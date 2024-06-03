82d0ae70130e18b90bf14063bda5e9b6082b86ab
SUREN S <121883751+SUREN239@users.noreply.github.com>
2024-06-03 06:30:40 +0530

    Update PuriHotelTest.java

--- Lab Question Answers/PuriHotelTest.java
+++ Lab Question Answers/PuriHotelTest.java
@@ -5,7 +5,7 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.Set;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.By; 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
